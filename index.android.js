6cd29db25efea879e3376138986ca521f78e022b
Quek Yang Sheng Tyson <quektyson@gmail.com>
2017-05-11 01:59:31 +0800

    Add a few props

--- index.android.js
+++ index.android.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
-import { AppRegistry, Text, Image } from 'react-native';
+import { AppRegistry, Text, Image, View } from 'react-native';
 
 class HelloWorld extends Component {
   render(){
     return (
-      <Text>Hello world!</Text>
-      <Bananas></Bananas>
+      <View style={{alignItems: 'center'}}>
+        <Text>Hello world!</Text>
+        <Bananas></Bananas>
+        <LotsOfGreeting></LotsOfGreeting>
+      </View>
+
     );
   }
 }
@@ -17,6 +21,27 @@ class Bananas extends Component {
     }
     return(
       <Image source={pic} style ={{width: 193, height: 100}}/>
+    );
+  }
+}
+
+class Greeting extends Component {
+  render(){
+    return(
+      <Text>Hello {this.props.name}! </Text>
+    );
+  }
+}
+
+class LotsOfGreeting extends Component {
+  render (){
+    return (
+      <View>
+        <Greeting name = 'Lorum'></Greeting>
+        <Greeting name = 'Ipsum'></Greeting>
+        <Greeting name = 'Dolor'></Greeting>
+      </View>
+
     );
   }
 }
